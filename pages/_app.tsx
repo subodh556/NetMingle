1907fa1b4819159e9be444113e33bb0ba159b4e0
Subodh <sohonisubodh@gmail.com>
2023-12-28 23:13:40 +0530

    upload image

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -5,11 +5,13 @@ import LoginModal from '@/components/Modals/LoginModal'
 import RegisterModal from '@/components/Modals/RegisterModal'
 import { Toaster } from 'react-hot-toast';
 import { SessionProvider } from 'next-auth/react';
+import EditModal from '@/components/Modals/EditModal'
 
 export default function App({ Component, pageProps }: AppProps) {
   return( 
     <SessionProvider session={pageProps.session}>
       <Toaster />
+      <EditModal/>
       <RegisterModal/>
       <LoginModal/>
       <Layout>

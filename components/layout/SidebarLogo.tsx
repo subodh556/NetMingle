75b2082f15e78c8830be858203feb90b939e5037
Subodh <sohonisubodh@gmail.com>
2024-01-11 23:04:30 +0530

    updated logo

--- components/layout/SidebarLogo.tsx
+++ components/layout/SidebarLogo.tsx
@@ -8,12 +8,12 @@ const SidebarLogo = () => {
   return (
     <div>
       <div className="relative rounded-full h-20 w-20 flex items-center justify-center p-4 hover:bg-slate-300 hover:bg-opacity-10 cursor-pointer lg:hidden">
-      <Image src={'/images/pp.png'} width={300} height={30} alt=""/>
+      <Image src={'/images/pii.png'} width={500} height={30} alt=""/>
 </div>
       <div  onClick={() => router.push('/')}
       className="relative hidden lg:flex items-row gap-4  p-4 rounded-full 
       cursor-pointer items-center ">
-            <Image src={'/images/post.png'} width={300} height={30} alt=""/>
+            <Image src={'/images/logg2.jpeg'} width={300} height={30} alt=""/>
       </div>
     </div>
     

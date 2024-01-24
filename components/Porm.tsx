e566fb2d363775ab265822cb1521ed4862485015
Subodh <sohonisubodh@gmail.com>
2024-01-24 18:50:11 +0530

    commit

--- components/Porm.tsx
+++ components/Porm.tsx
@@ -14,7 +14,17 @@ const Porm = () => {
         return null;
     }else{
         return(
-           
+            <div className="py-2 ">
+        <h1 className="text-white text-2xl text-center mb-4 font-bold">
+        👋 Welcome to <span className='text-customBlue'>Post It!</span> 🚀
+        </h1>
+        <p className="text-white text-1xl mb-7 mx-6">Discover the power of concise expression! PostIt is your go-to destination for sharing thoughts, connecting with friends, and engaging with a vibrant community! </p>
+    
+        <div className="flex flex-row items-center justify-center gap-4">
+          <Button label="Login" onClick={loginModal.onOpen} />
+          <Button label="Register" onClick={registerModal.onOpen} secondary />
+        </div>
+      </div>
         )
     }
     

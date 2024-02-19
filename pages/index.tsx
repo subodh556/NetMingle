2c944d68c311266e7754f4a58c81f6d3f67c4112
Subodh <sohonisubodh@gmail.com>
2024-02-19 22:27:34 +0530

    commit

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,17 +3,21 @@ import Header from "@/components/Header";
 
 import PostFeed from "@/components/posts/PostFeed";
 import useCurrentUser from "@/hooks/useCurrentUser";
-import Porm from "../components/Porm";
+import Porm from "@/components/Porm";
+
+
 
 
 
 export default function Home() {
   const currentUser=useCurrentUser();
+  const visi=currentUser?false:true;
+  
   return (
     <>
       <Header label="Home"/>
       
-      <Porm />
+      {visi && <Porm/>}
       <div className="text-customBlue text-xl font-bold p-4 px-6"> Recent Posts </div>
       <PostFeed/>
     </>

97d0ffdba6f0bbbd93526c9b39e1ed93061107a9
Subodh <sohonisubodh@gmail.com>
2024-02-21 00:14:00 +0530

    profile

--- components/Avatar.tsx
+++ components/Avatar.tsx
@@ -8,9 +8,10 @@ interface AvatarProps {
   userId: string;
   isLarge?: boolean;
   hasBorder?: boolean;
+  isSmall?:boolean;
 }
 
-const Avatar: React.FC<AvatarProps> = ({ userId, isLarge, hasBorder }) => {
+const Avatar: React.FC<AvatarProps> = ({ userId, isLarge, hasBorder,isSmall }) => {
   const router = useRouter();
 
   const { data: fetchedUser } = useUser(userId);
@@ -26,8 +27,8 @@ const Avatar: React.FC<AvatarProps> = ({ userId, isLarge, hasBorder }) => {
   return (
     <div className={`
         ${hasBorder ? 'border-4 border-black' : ''}
-        ${isLarge ? 'h-32' : 'h-12'}
-        ${isLarge ? 'w-32' : 'w-12'}
+        ${isLarge ? 'h-32' : isSmall?'h-8':'h-12'}
+        ${isLarge ? 'w-32' : isSmall?'w-8':'w-12'}
         rounded-full  hover:opacity-90 transition cursor-pointer relative`}>
       <Image
         fill
--- components/layout/SidebarItem.tsx
+++ components/layout/SidebarItem.tsx
@@ -4,6 +4,7 @@ import { useRouter } from 'next/router';
 import React, { useCallback} from 'react'
 import { IconType } from 'react-icons';
 import { BsDot } from 'react-icons/bs';
+import Avatar from '../Avatar';
 
 interface SidebarItemProps {
     label: string;
@@ -26,6 +27,7 @@ const SidebarItem:React.FC<SidebarItemProps> = ({
     if(auth && !currentuser){
         loginModal.onOpen();
     }
+    
     else if(href){
         router.push(href);
     }
@@ -34,16 +36,29 @@ const SidebarItem:React.FC<SidebarItemProps> = ({
 
   return (
     <div onClick={handlClick} className="flex flex-row items-center">
-         <div className="relative rounded-full h-14 w-14 flex items-center justify-center p-4 hover:bg-slate-300 hover:bg-opacity-10 cursor-pointer lg:hidden">
-            <Icon size={28} color="white" />
+         <div className="relative rounded-full h-20 w-20 flex items-center justify-center p-4 hover:bg-slate-300 hover:bg-opacity-10 cursor-pointer lg:hidden">
+         {(currentuser && label==="Profile") ?
+            <><Avatar userId={currentuser.id} isSmall />
+            
+        </>:<><Icon size={29} color="white" />
+            
+            </>}
             {alert ? <BsDot className="text-sky-500 absolute -top-4 left-0" size={70} /> : null}
         </div>
-        <div className="relative hidden lg:flex items-row gap-4  p-4 rounded-full hover:bg-slate-300 
+        <div className="relative hidden lg:flex items-row gap-5 w-80 p-1 py-3 rounded-lg hover:bg-slate-300 
         hover:bg-opacity-10 cursor-pointer items-center">
-            <Icon size={24} color="white" />
+            {(currentuser && label==="Profile") ?
+            <><Avatar userId={currentuser.id} isSmall/>
+            <p className="hidden lg:block text-white text-xl">
+                {currentuser.name}
+            </p>
+        </>:<><Icon size={29} color="white" />
             <p className="hidden lg:block text-white text-xl">
                 {label}
             </p>
+            </>}
+            
+            
             {alert ? <BsDot className="text-sky-500 absolute -top-4 left-0" size={70} /> : null}
         </div>
     </div>  

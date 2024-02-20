d9dd67450b517ad0613b792986f921946955c15f
Subodh <sohonisubodh@gmail.com>
2024-02-20 08:53:50 +0530

    logo

--- components/layout/FollowBar.tsx
+++ components/layout/FollowBar.tsx
@@ -19,12 +19,12 @@ const FollowBar = () => {
               {users.map((user: Record<string, any>) => (
               
               <div key={user.id} className="flex flex-row gap-4">
-              <Avatar userId={user.id} />
-              <div className="flex flex-col">
-                <p className="text-white font-semibold text-sm">{user.name}</p>
-                <p className="text-neutral-400 text-sm">@{user.username}</p>
+                <Avatar userId={user.id} />
+                <div className="flex flex-col">
+                  <p className="text-white font-semibold text-sm">{user.name}</p>
+                  <p className="text-neutral-400 text-sm">@{user.username}</p>
+                </div>
               </div>
-            </div>
               ))}
             </div>
         </div>
--- components/layout/SidebarLogo.tsx
+++ components/layout/SidebarLogo.tsx
@@ -7,7 +7,7 @@ const SidebarLogo = () => {
   const router=useRouter();
   return (
     <div>
-      <div className="relative rounded-full h-20 w-20 flex items-center justify-center p-4 hover:bg-slate-300 hover:bg-opacity-10 cursor-pointer lg:hidden">
+      <div onClick={()=>router.push('/')} className="relative rounded-full h-20 w-20 flex items-center justify-center p-4 hover:bg-slate-300 hover:bg-opacity-10 cursor-pointer lg:hidden">
       <Image src={'/images/pii.png'} width={500} height={30} alt=""/>
 </div>
       <div  onClick={() => router.push('/')}
